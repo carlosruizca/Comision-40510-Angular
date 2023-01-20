f36a20bf6700f22d6685de856bd97af95cd4a220
Carlos Ruiz <ce.ruiz2203@gmail.com>
2023-01-19 22:04:29 -0500

    Subiendo archivos entrega 1

---
+++ ComponentesRuiz/Entrega/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { LoginComponent } from './components/login/login.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductosComponent,
+    CarritoComponent,
+    LoginComponent,
+    BlogComponent,
+    ToolbarComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
